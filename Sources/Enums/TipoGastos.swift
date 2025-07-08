da1f81ebd9da211fb5e93fd4c035f539b491eab4
Abel Arley <abelarleyy@gmail.com>
2025-07-08 13:28:03 -0300

    Adicionado as funções consulta por gasto, calcular gasto total, calcular receita total, remover gasto, editar gasto, remover receita, editar receita, media de gastos

--- Sources/Enums/TipoGastos.swift
+++ Sources/Enums/TipoGastos.swift
@@ -2,5 +2,5 @@ enum TipoGastos: String, CaseIterable{
     case comida = "Comida"
     case lazer = "Lazer"
     case transporte = "Transporte"
-    case compras = "Compras"
+    case compra = "Compra"
 }
--- Sources/Models/main.swift
+++ Sources/Models/main.swift
@@ -34,13 +34,103 @@ class GerenciadorOrcamento{
         }
         print("Receita não encontrada!")
     }
+    func consultaPorGasto(id: UUID){
+        for gasto in gastos{
+            if gasto.id == id{
+                print("Gasto encontrado: \(gasto.descricaoFormatada())")
+                return
+            }
+        }
+    }
+    func calcularGastoTotal() -> Double{
+        var total = 0.0
+        for gasto in gastos{
+            total += gasto.valor
+        }
+        return total
+    }
+
+    func calcularReceitaTotal() -> Double{
+        var total = 0.0
+        for receita in receitas{
+            total += receita.valor
+        }
+        return total
+    }
+    
+    func removerGasto(id: UUID){
+        if let index = gastos.firstIndex(where: { $0.id == id }) {
+            let gastoRemovido = gastos.remove(at: index)
+            saldo += gastoRemovido.valor
+            print("Gasto \(gastoRemovido.descricao) removido com sucesso")
+        } else {
+        print("Gasto não encontrado!")
+        }
+    }
+    func editarGasto(id: UUID, novoValor: Double, novaDescricao: String, novaCategoria: TipoGastos){
+        if let index = gastos.firstIndex(where: { $0.id == id }) {
+            let gastoEditado = gastos[index]
+            gastoEditado.valor = novoValor
+            gastoEditado.descricao = novaDescricao
+            gastoEditado.categoria = novaCategoria.rawValue
+            print("Gasto \(gastoEditado.descricao) editado com sucesso")
+        } else {
+            print("Gasto não encontrado!")
+        }
+    }
+    func removerReceita(id: UUID){
+        if let index = receitas.firstIndex(where: { $0.id == id }){
+            let receitaRemovida = receitas.remove(at: index)
+            saldo -= receitaRemovida.valor
+            print("Receita \(receitaRemovida.descricao) removida com sucesso")
+        } else {
+            print("Receita não encontrada!")
+        }
+    }
+    func editarReceita(id: UUID, novoValor: Double, novaDescricao: String, novaCategoria: TiposReceitas){
+        if let index = receitas.firstIndex(where:{ $0.id == id}){
+            let receitaEditada = receitas[index]
+            receitaEditada.valor = novoValor
+            receitaEditada.descricao = novaDescricao
+            receitaEditada.categoria = novaCategoria.rawValue
+            print("Receita \(receitaEditada.descricao) editada com sucesso")
+        } else {
+            print("Receita não encontrada!")
+        }
+    }
+    func mediaGastos() -> Double{
+        if gastos.count == 0{
+            return 0.0
+        }
+        return calcularGastoTotal() / Double(gastos.count)
+    }
 }
+
 let receita1 = ReceitaPessoal(valor: 150.0, descricao: "Trabalho de Social Media", categoria: .freelance)
+let receita2 = ReceitaPessoal(valor: 245.3, descricao: "Tesouro Nacional", categoria: .investimento)
 let gasto1 = GastoPessoal(valor: 50.0, descricao: "Mercado", categoria: .comida)
+let gasto2 = GastoPessoal(valor: 132.50, descricao: "Compra Online", categoria: .compra)
 
 let meuGerenciador = GerenciadorOrcamento()
 meuGerenciador.adicionarReceita(novaReceita: receita1)
+meuGerenciador.adicionarReceita(novaReceita: receita2)
 meuGerenciador.adicionarGasto(novoGasto: gasto1)
+meuGerenciador.adicionarGasto(novoGasto: gasto2)
 
 print("Saldo atual: \(meuGerenciador.saldo)")
-print(meuGerenciador.consultaPorReceita(id: receita1.id))
+//print(meuGerenciador.consultaPorReceita(id: receita1.id))
+//meuGerenciador.removerGasto(id: gasto1.id)
+//print("Gasto total: \(meuGerenciador.calcularGastoTotal())")
+//print("Receita total: \(meuGerenciador.calcularReceitaTotal())")
+//print("Gasto 2: \(gasto2.descricaoFormatada())")
+//meuGerenciador.editarGasto(id: gasto2.id, novoValor: 100.00, novaDescricao: "Compra no Mercado", novaCategoria: .comida)
+//print("Gasto total: \(meuGerenciador.calcularGastoTotal())")
+//print("Gasto 2: \(gasto2.descricaoFormatada())")
+
+//meuGerenciador.removerReceita(id: receita1.id)
+//print("Saldo atual: \(meuGerenciador.saldo)")
+//meuGerenciador.editarReceita(id: receita2.id, novoValor: 240.5, novaDescricao: "Tesouro Internacional", novaCategoria: .cashback)
+//print("Saldo atual: \(meuGerenciador.saldo)")
+//print("Receita 2: \(receita2.descricaoFormatada())")
+
+print("Média de gastos: \(meuGerenciador.mediaGastos())")
